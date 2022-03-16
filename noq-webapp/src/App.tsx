110b7bf6901f9fcaad5998117ab0804b1105e324
kotharismeeet <smeetkothari@FVFGW1AYQ05N.local>
2022-03-16 18:49:05 +0530

    skeleton

---
+++ noq-webapp/src/App.tsx
@@ -0,0 +1,10 @@
+import CustomButton from "./components/utils/CustomButton";
+import './components/utils/CustomButton.scss';
+
+function App() {
+  return (<div className="center">
+  <CustomButton />
+  </div>);
+}
+
+export default App;
---
+++ noq-webapp/src/components/utils/CustomButton.tsx
@@ -0,0 +1,12 @@
+import Button from '@mui/material/Button';
+//import {ThemeProvider} from '@mui/material/styles';
+//import theme from './muiTheme';
+import './CustomButton.scss';
+
+const CustomButton = () => {    
+  return (
+    <Button className="button">Button</Button>
+    );
+};
+
+export default CustomButton;
